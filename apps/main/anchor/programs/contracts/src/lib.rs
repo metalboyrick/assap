125417ff44d161b459a5b4f8066b3789eaef52fd
metalboyrick <rsulisthio@gmail.com>
2025-04-26 19:54:12 +0700

    chore: program id update

--- apps/main/anchor/programs/contracts/src/lib.rs
+++ apps/main/anchor/programs/contracts/src/lib.rs
@@ -2,7 +2,7 @@
 
 use anchor_lang::prelude::*;
 
-declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+declare_id!("4PbTBdcZP5CHTVzmQTNpQzGeLHkVvMAdhu7TopkjtQ4e");
 
 mod error_code;
 mod verifiers;
