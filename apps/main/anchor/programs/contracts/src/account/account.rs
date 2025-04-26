0ec3a5e744d949e16401e192155f53de06df677d
metalboyrick <rsulisthio@gmail.com>
2025-04-26 18:32:55 +0700

    feat: add data_cid to user account

--- apps/main/anchor/programs/contracts/src/account/account.rs
+++ apps/main/anchor/programs/contracts/src/account/account.rs
@@ -12,6 +12,8 @@ pub struct User {
     pub email_account: Pubkey,
     pub discord_account: Pubkey,
     pub human_verification: bool,
+
+    /// points to additional data in IPFS
     pub data_cid: String,
 }
 
