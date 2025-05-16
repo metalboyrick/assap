4f3730088ed9c84f55f647efc58dce19d2c55eea
metalboyrick <rsulisthio@gmail.com>
2025-05-16 21:45:18 +0700

    feat: update button props

--- packages/assap-sdk/src/components/AttestButton.tsx
+++ packages/assap-sdk/src/components/AttestButton.tsx
@@ -1,15 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { AttestationData } from "@/core";
 import { useAssapAttest } from "@/hooks/useAssapAttest";
+import { cn } from "@/lib/utils";
+import { Cluster } from "@solana/web3.js";
 
 export function AttestButton({
   schemaId,
   onAttestComplete,
   attestData,
+  className,
 }: {
   schemaId: string;
   onAttestComplete: () => void;
   attestData: AttestationData;
+  className?: string;
+  cluster: Cluster;
 }) {
   const { initiateAttestation } = useAssapAttest({
     schemaId,
@@ -24,7 +29,10 @@ export function AttestButton({
           attestData,
         })
       }
-      className="bg-gradient-to-r from-red-600 to-blue-600 hover:from-red-700 hover:to-blue-700"
+      className={cn(
+        "bg-gradient-to-r from-red-600 to-blue-600 hover:from-red-700 hover:to-blue-700",
+        className,
+      )}
     >
       Attest Now
     </Button>
